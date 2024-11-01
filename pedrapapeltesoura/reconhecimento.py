83a2a0fac5344dd74e6bbdc339070f6b6bd24230
lucasttmz <lucasttmz@gmail.com>
2024-10-31 22:50:41 -0300

    Melhorada a lógica de reconhecimento

--- pedrapapeltesoura/reconhecimento.py
+++ pedrapapeltesoura/reconhecimento.py
@@ -2,38 +2,48 @@ import mediapipe as mp
 
 from constantes import Escolha, Jogadores
 
-def reconhecer_gesto(landmarks) -> tuple[Escolha, Jogadores] | None:
-    indicador_ponta = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP] # type: ignore
-    indicador_meio = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_PIP] # type: ignore
-    polegar_ponta = landmarks[mp.solutions.hands.HandLandmark.THUMB_TIP] # type: ignore
-    polegar_meio = landmarks[mp.solutions.hands.HandLandmark.THUMB_MCP] # type: ignore
-    dedo_meio_ponta = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP] # type: ignore
-    dedo_meio_meio = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_PIP] # type: ignore
-    anelar_ponta = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_TIP] # type: ignore
-    anelar_meio = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_PIP] # type: ignore
-    mindinho_ponta = landmarks[mp.solutions.hands.HandLandmark.PINKY_TIP] # type: ignore
-    mindinho_meio = landmarks[mp.solutions.hands.HandLandmark.PINKY_PIP] # type: ignore
-    punho = landmarks[mp.solutions.hands.HandLandmark.WRIST] # type: ignore
 
+def reconhecer_gesto(landmarks) -> tuple[Escolha, Jogadores] | None:
+    indicador_ponta = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
+    indicador_meio = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_PIP]
+    polegar_ponta = landmarks[mp.solutions.hands.HandLandmark.THUMB_TIP]
+    polegar_meio = landmarks[mp.solutions.hands.HandLandmark.THUMB_IP]
+    dedo_meio_ponta = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP]
+    dedo_meio_meio = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_PIP]
+    anelar_ponta = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_TIP]
+    anelar_meio = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_PIP]
+    mindinho_ponta = landmarks[mp.solutions.hands.HandLandmark.PINKY_TIP]
+    mindinho_meio = landmarks[mp.solutions.hands.HandLandmark.PINKY_PIP]
+    punho = landmarks[mp.solutions.hands.HandLandmark.WRIST]
+
+    # Verificação do jogador 1 (apontando para a direita)
     if punho.x < indicador_ponta.x:
         if (indicador_ponta.x > indicador_meio.x and dedo_meio_ponta.x > dedo_meio_meio.x and 
-            anelar_ponta.x > anelar_meio.x and mindinho_ponta.x > mindinho_meio.x):
+            anelar_ponta.x > anelar_meio.x and mindinho_ponta.x > mindinho_meio.x and 
+            polegar_ponta.y < polegar_meio.y):
             return Escolha.PAPEL, Jogadores.JOGADOR1
-        elif indicador_ponta.x > indicador_meio.x and dedo_meio_ponta.x > dedo_meio_meio.x:
+        elif (indicador_ponta.x > indicador_meio.x and dedo_meio_ponta.x > dedo_meio_meio.x and
+            anelar_ponta.x <= anelar_meio.x and mindinho_ponta.x <= mindinho_meio.x and
+            polegar_ponta.y >= polegar_meio.y):
             return Escolha.TESOURA, Jogadores.JOGADOR1
         elif (indicador_ponta.x <= indicador_meio.x and dedo_meio_ponta.x <= dedo_meio_meio.x and 
             anelar_ponta.x <= anelar_meio.x and mindinho_ponta.x <= mindinho_meio.x and
-            (polegar_ponta.y * 720 + 25) >= (polegar_meio.y * 720)):
+            polegar_ponta.y >= polegar_meio.y):
             return Escolha.PEDRA, Jogadores.JOGADOR1
+    # Verificação do jogador 2 (apontando para a esquerda)
     else:
         if (indicador_ponta.x < indicador_meio.x and dedo_meio_ponta.x < dedo_meio_meio.x and 
-            anelar_ponta.x < anelar_meio.x and mindinho_ponta.x < mindinho_meio.x):
+            anelar_ponta.x < anelar_meio.x and mindinho_ponta.x < mindinho_meio.x and
+            polegar_ponta.y < polegar_meio.y):
             return Escolha.PAPEL, Jogadores.JOGADOR2
-        elif indicador_ponta.x < indicador_meio.x and dedo_meio_ponta.x < dedo_meio_meio.x:
+        elif (indicador_ponta.x < indicador_meio.x and dedo_meio_ponta.x < dedo_meio_meio.x and
+              anelar_ponta.x >= anelar_meio.x and mindinho_ponta.x >= mindinho_meio.x and
+              polegar_ponta.y >= polegar_meio.y):
             return Escolha.TESOURA, Jogadores.JOGADOR2
         elif (indicador_ponta.x >= indicador_meio.x and dedo_meio_ponta.x >= dedo_meio_meio.x and 
             anelar_ponta.x >= anelar_meio.x and mindinho_ponta.x >= mindinho_meio.x and
-            (polegar_ponta.y * 720 + 25) >= (polegar_meio.y * 720)):
+            polegar_ponta.y >= polegar_meio.y):
             return Escolha.PEDRA, Jogadores.JOGADOR2
 
+    # Se chegar aqui, não reconheceu nenhum gesto
     return None
